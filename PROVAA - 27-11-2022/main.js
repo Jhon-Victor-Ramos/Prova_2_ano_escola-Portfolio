47f01a732929be5dbc2456ed6f0a8a82e656253f
Jhon Victor <jhonvictorcraft@gmail.com>
2022-11-27 00:36:30 -0300

    Site da prova :)

---
+++ PROVAA - 27-11-2022/main.js
@@ -0,0 +1,61 @@
+// SMOOTH SCROLL
+
+const menuLinks = document.querySelectorAll('.dp-menu a[href^="#"], .menu a[href^="#"]');
+
+function getDistanceFromTheTop(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 33;
+  smoothScrollTo(0, distanceFromTheTop);
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
+
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== "undefined" ? duration : 600;
+
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1)
+      return (distance / 2) * time * time * time * time + from;
+    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60);
+}
+
+// ALERT
+
+function alerta() {
+    swal({
+        title: "Olá!",
+        text: "Este site não está disponivel no momento! Tente os outros",
+        icon: "error",
+    });
+}
+function alerta2() {
+    swal({
+        title: "Olá!",
+        text: "Você já está nesse site! Tente os outros",
+        icon: "warning",
+    });
+}
